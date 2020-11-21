5f78afc27927a46e5ae744d2a475419f2dc1ce7a
IvanHrIvanov <vinko96@abv.bg>
2020-11-21 21:57:00 +0200

    Add

---
+++ 04. Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string output = string.Empty;
+            foreach (var item in input)
+            {
+
+                output += (char)(item + 3);
+
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
