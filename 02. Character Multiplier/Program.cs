098e2ae2ecc65f464f5348ffa25e9e820c6cbc2d
IvanHrIvanov <vinko96@abv.bg>
2020-11-21 21:45:01 +0200

    Add

---
+++ 02. Character Multiplier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstWord = input[0];
+            string secondWod = input[1];
+
+            string longWord = firstWord;
+            string shortWOrd = secondWod;
+            
+            if(firstWord.Length<secondWod.Length)
+            {
+                longWord = secondWod;
+                shortWOrd = firstWord;
+            }
+
+            int totalSUm = CharManipulater(longWord, shortWOrd);
+            Console.WriteLine(totalSUm);
+
+        }
+        public static int CharManipulater(string longWord, string shortWord)
+        {
+            int sum = 0;
+            for (int i = 0; i < shortWord.Length; i++)
+            {
+                int multiply = longWord[i] * shortWord[i];
+                sum += multiply;
+            }
+            for (int i = shortWord.Length; i < longWord.Length; i++)
+            {
+                sum += longWord[i];
+            }
+            return sum;
+        }
+        
+    }
+}
---
+++ 03. Extract File/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(@"\");
+
+            string[] lastFile = input[input.Length-1].Split(".");
+
+            Console.WriteLine($"File name: {lastFile[0]}");
+            Console.WriteLine($"File extension: {lastFile[1]}");
+        }
+    }
+}
